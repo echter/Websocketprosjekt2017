e8adc16d4a24c13d44fb8352aea53aa5d261aab7
Elias <eliassorr@gmail.com>
2017-04-25 15:01:50 +0200

    Renamed interface files for setting up functional interfaces

--- Websocket/src/main/java/no/ntnu/stud/websocket/server/interfaces/functionalinterfaces/ConnectionListener.java
+++ Websocket/src/main/java/no/ntnu/stud/websocket/server/interfaces/functionalinterfaces/ConnectionListener.java
@@ -3,6 +3,7 @@ package no.ntnu.stud.websocket.server.interfaces.functionalinterfaces;
 /**
  * Created by EliasBrattli on 25/04/2017.
  */
-public interface Listener {
-    void connect();
+@FunctionalInterface
+public interface ConnectionListener {
+    void onAction();
 }
